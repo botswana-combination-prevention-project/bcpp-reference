e482142f1e69d88f6fb7848d1e81b8ce6c2033f8
erikvw <ew2789@gmail.com>
2017-07-17 15:22:16 +0200

    initial

---
+++ bcpp_reference/reference_fields.py
@@ -0,0 +1,20 @@
+from edc_reference.site import site_reference_configs
+from edc_visit_schedule.site_visit_schedules import site_visit_schedules
+
+site_reference_configs.register_from_visit_schedule(
+    site_visit_schedules=site_visit_schedules)
+
+configs = {
+    'bcpp_subject.HivResult': ['hiv_result', 'hiv_result_datetime'],
+    'bcpp_subject.HivCareAdherence': [
+        'arv_evidence', 'ever_taken_arv', 'arv_evidence', 'on_arv'],
+    'bcpp_subject.ElisaHivResult': ['hiv_result', 'hiv_result_datetime'],
+    'bcpp_subject.hivtestinghistory': [
+        'verbal_hiv_result', 'other_record', 'has_tested'],
+    'bcpp_subject.hivtestreview': ['hiv_test_date', 'recorded_hiv_result'],
+    'bcpp_subject.hivresultdocumentation': [
+        'result_date', 'result_recorded', 'result_doc_type'],
+}
+for model, fields in configs.items():
+    site_reference_configs.add_fields_to_config(
+        model, fields)
---
+++ bcpp_reference/tests/test_reference.py
@@ -0,0 +1,8 @@
+from django.test import TestCase, tag
+
+from edc_reference.site import site_reference_configs
+
+
+class TesReference(TestCase):
+    def test_(self):
+        site_reference_configs
